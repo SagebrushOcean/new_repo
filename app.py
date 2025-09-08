49194b186ab0fbd709fefdedd7c7cfa62cb3dd54
SagebrushOcean <katya.shevchenko.2014@inbox.ru>
2025-09-08 19:43:36 +0300

    Задание 2

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
 from flask import Flask
 
-# Создаем объект приложения Flask
 app = Flask(__name__)
 
-# Определяем маршрут и привязываем его к функции
 @app.route('/')
-def hello():
-    return "Hello, Flask!"
+def home():
+    return "This is the homepage"
 
-# Запуск приложения
+#1
+@app.route("/hello")
+def hello():
+    return "Hello, world!"
+
+@app.route("/info")
+def info():
+    return "This is an informational page."
+
+#2
+@app.route("/calc/<num1>/<num2>")
+def calc(num1, num2):
+    try:
+        return f"The sum of {num1} and {num2} is {int(num1)+int(num2)}."
+    except ValueError:
+        return f"Ошибка: входные данные должны быть целыми числами."
+
+#3
+@app.route("/reverse/<string>")
+def reverse(string):
+    if string.strip():
+        l = list(string)
+        l.reverse()
+        return f"{''.join(l)}"
+    return f"Ошибка: введите хотя бы один символ."
+
+#4
+@app.route("/user/<name>/<int:age>")
+def user(name, age):
+    if age>=0:
+        return f"Hello, {name}. You are {age} years old."
+    return f"Ошибка: некорректный возраст."
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
