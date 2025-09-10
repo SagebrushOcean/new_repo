6f95713c9014b2662f92a0a8f228d881a7df60c3
SagebrushOcean <katya.shevchenko.2014@inbox.ru>
2025-09-10 15:19:40 +0300

    Задание 3

---
+++ flask_project/app/routes.py
@@ -0,0 +1,34 @@
+from app import app
+
+@app.route('/')
+def home():
+    return "This is the homepage"
+
+@app.route("/hello")
+def hello():
+    return "Hello, world!"
+
+@app.route("/info")
+def info():
+    return "This is an informational page."
+
+@app.route("/calc/<num1>/<num2>")
+def calc(num1, num2):
+    try:
+        return f"The sum of {num1} and {num2} is {int(num1)+int(num2)}."
+    except ValueError:
+        return f"Ошибка: входные данные должны быть целыми числами."
+
+@app.route("/reverse/<string>")
+def reverse(string):
+    if string.strip():
+        l = list(string)
+        l.reverse()
+        return f"{''.join(l)}"
+    return f"Ошибка: введите хотя бы один символ."
+
+@app.route("/user/<name>/<int:age>")
+def user(name, age):
+    if age>=0:
+        return f"Hello, {name}. You are {age} years old."
+    return f"Ошибка: некорректный возраст."
