272615eb1c401b4c6757eb46affe4fde7a9d81b8
SagebrushOcean <katya.shevchenko.2014@inbox.ru>
2025-09-18 17:38:54 +0300

    Практикум 1

--- flask_project/app/routes.py
+++ flask_project/app/routes.py
@@ -1,22 +1,23 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 
+#Главная
 @app.route("/")
-def form():
-    return render_template("form.html")
+def index():
+    return render_template("index.html")
 
-@app.route("/submit", methods=["POST", "GET"])
+#Контакты
+@app.route("/contact", methods=["POST", "GET"])
 def submit():
     if request.method == "POST":
         name = request.form.get("name")
-        color = request.form.get("color")
-        profession = request.form.get("profession")
-        hobbies = request.form.getlist("hobbies")
-        level = request.form.get("level")
-        return render_template("result.html", name=name, color=color, profession=profession, hobbies=hobbies, level=level)
+        email = request.form.get("email")
+        message = request.form.get("message")
+        return render_template("contact.html", status='')
     else:
-        return redirect(url_for("form"))
+        return render_template("contact.html", status='hide')
 
-@app.route("/game")
-def game():
-    return render_template("game.html")
+#О нас
+@app.route("/about")
+def about():
+    return render_template("about.html")
--- flask_project/app/static/js/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('secret-button').addEventListener('click', function(event) {
-    document.getElementById('secret-message').classList.toggle('hide');
-});
